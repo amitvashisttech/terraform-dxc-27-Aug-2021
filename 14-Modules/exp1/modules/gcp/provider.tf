176acff87a27e5769996f6acb89c789a0f3e9345
root <root@your.domain.com>
2021-08-31 12:17:34 +0000

    14-Modules

---
+++ 14-Modules/exp1/modules/gcp/provider.tf
@@ -0,0 +1,6 @@
+provider "google" {
+  project     = var.project
+  credentials = file("/root/.ssh/account.json")
+  region      = var.region
+}
+
---
+++ 14-Modules/exp1/variable.tf
@@ -0,0 +1,14 @@
+variable "frontend_instance_count" { 
+   default = 2  
+}
+
+
+
+variable "backend_instance_count" { 
+   default = 2
+}
+
+
+variable "backend_region" {
+   default = "us-central1"
+}
